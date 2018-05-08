a960841bcb1bda2079e83a944d87afefccf7013d
Tiago Fraga <tiagofraga@MBP-de-Tiago.home>
2018-05-08 02:15:00 +0100

    Adicionei o trabalho com os dois primeiros pontos feitos

--- notebook.c
+++ notebook.c
@@ -27,94 +27,103 @@ char *lerLinha(int fd){
 int main(){
 	
 	char *linha, *linha_aux, *args[MAX_ARGS], *token;
-	int status, i, fd_aux, fd;
+	int status, i, fd;
 	int comandos = 0;
-	int nivel = 0;
+	int file, nivel = 0;
 
-	fd_aux = open("teste.txt", O_RDONLY);
-
-	if(fd_aux!=-1){
-
-		while((linha_aux = lerLinha(0))!=NULL){
-			if(linha_aux[0] == '$'){
-				comandos++;
-			}
-		}
-		close(fd_aux);
-	}
-
-	int pipes[comandos][2];
+	int p[2];
 
 	fd = open("teste.txt", O_RDONLY);
 
+
 	if(fd!=-1){
 		while((linha = lerLinha(fd))!=NULL){
-			if (linha[0] == '$' && linha[1] == '|'){
-				printf("$%s\n", linha + 1);
+			if (linha[0] == '$' && linha[1] == '|'){	
 				
+				printf("$|%s\n", linha + 2);
+
+				for(token = strtok(linha+2, " "), i = 0; token; token = strtok(NULL, " "), i++){
+					args[i] = token;
+				}
+					
+				args[i] = NULL;
+				printf(">>>\n");
+
+				pipe(p);
+
+				if(fork()==0){
+					close(p[0]);
+					file = open("tmp", O_CREAT|O_RDONLY);
+					dup2(p[1], 1);
+					dup2(file, 0);
+					execvp(args[0], args);
+					close(p[1]);
+					close(file);
+				}
+
+
+				wait(&status);
+				close(p[1]);
+				file = open("tmp",O_CREAT| O_TRUNC | O_WRONLY, S_IRWXU | S_IRWXG | S_IRWXO);
+
+				while((linha = lerLinha(p[0]))){
+					write(1, linha, strlen(linha));
+					write(file, linha, strlen(linha));
+					write(file,"\n",1);
+					write(1,"\n",1);
+				}	
+
+				close(p[0]);
+				close(file);
+				printf("<<<\n");
+			}	
+			else if (linha[0] == '$'){
+
+				printf("$%s\n", linha + 1);
+
 				for(token = strtok(linha+1, " "), i = 0; token; token = strtok(NULL, " "), i++){
 					args[i] = token;
 				}
-
+					
 				args[i] = NULL;
 				printf(">>>\n");
-				pipe(pipes[nivel]);
+
+				pipe(p);
 
 				if(fork()==0){
-					dup2(0,pipes[nivel-1][0]);
+					close(p[0]);
+					dup2(p[1], 1);
 					execvp(args[0], args);
-					dup2(pipes[nivel][1],1);
-					close(pipes[nivel][1]);
-					_exit(0);
+					close(p[1]);
 				}
 
-				close(pipes[nivel][1]);
 				wait(&status);
+				close(p[1]);
 
-				while((linha = lerLinha(pipes[nivel][0]))){
-					printf("%s\n", linha);
-				}
-							
-				close(pipes[nivel][0]);
-				nivel++;
+				file = open("tmp",O_CREAT| O_TRUNC | O_WRONLY, S_IRWXU | S_IRWXG | S_IRWXO);
+
+				while((linha = lerLinha(p[0]))){
+					write(1, linha, strlen(linha));
+					write(file, linha, strlen(linha));
+					write(file,"\n",1);
+					write(1,"\n",1);
+				}	
+
+				close(p[0]);
+				close(file);
 				printf("<<<\n");
-			}
-			else if (linha[0] == '$'){
-
-					printf("$%s\n", linha + 1);
-					for(token = strtok(linha+1, " "), i = 0; token; token = strtok(NULL, " "), i++){
-						args[i] = token;
-					}
-
-					args[i] = NULL;
-					printf(">>>\n");
-					pipe(pipes[nivel]);
-
-					if(fork() == 0){
-						execvp(args[0], args);
-						dup2(pipes[nivel][1],1);
-						close(pipes[nivel][1]);
-						_exit(0);
-					}
-
-					close(pipes[nivel][1]);
-					wait(&status);
-
-					while((linha = lerLinha(pipes[nivel][0]))){
-						printf("%s\n", linha);
-					}
-							
-					close(pipes[nivel][0]);
-					nivel++;
-					printf("<<<\n");
+					
 			}
 			else{
 				printf("%s\n", linha);
 			}
 		}
-	}else{
+	}
+	else {
 		perror("ERRO AO LER O FICHEIRO");
 	}
+
+	return 0;
 }
 
 	
